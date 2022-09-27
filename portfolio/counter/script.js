8762ba70aac0431ac8f07bd1b8a8b2fa4abe17ee
Kamil <jolki33.km@gmail.com>
2022-09-27 21:05:57 +0200

    first commit

---
+++ portfolio/counter/script.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrase')) {
+            count--;
+        }
+        else if(styles.contains('incrase')) {
+            count++;
+        } 
+        else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0){
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    });
+});
