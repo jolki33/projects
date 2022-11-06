4079306a789fdb87d9207ebf1e257625af1d053c
Kamil <jolki33.km@gmail.com>
2022-11-06 14:09:52 +0100

    timer next 2

--- portfolio/timer/script.js
+++ portfolio/timer/script.js
@@ -5,7 +5,7 @@ const resetBtb = document.querySelector(".reset");
 const historyBtb = document.querySelector(".history");
 const stopwatch = document.querySelector(".stopwatch");
 const time = document.querySelector(".time");
-const timeListBtb = document.querySelector(".time-list");
+const timeList = document.querySelector(".time-list");
 
 const infoBtb = document.querySelector(".info");
 const modalShadow = document.querySelector(".modal-shadow");
@@ -15,7 +15,10 @@ let countTime;
 let minutes = 0;
 let seconds = 0;
 
+let timesArr = [];
+
 const handleStart = () => {
+  clearInterval(countTime);
   countTime = setInterval(() => {
     if (seconds < 9) {
       seconds++;
@@ -31,6 +34,48 @@ const handleStart = () => {
   }, 100);
 };
 
-const handlePause
+const handlePause = () => {
+  clearInterval(countTime);
+};
+
+const handleStop = () => {
+  time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`;
+
+  if (stopwatch.textContent !== "0:00") {
+    time.style.visibility = "visible";
+    timesArr.push(stopwatch.textContent);
+    console.log(timesArr);
+  }
+
+  clearStuff();
+};
+
+const handleReset = () => {
+  time.style.visibility = "hidden";
+  timesArr = [];
+  clearStuff();
+};
+
+const clearStuff = () => {
+  clearInterval(countTime);
+  stopwatch.textContent = "0:00";
+  timeList.textContent = "";
+  seconds = 0;
+  minutes = 0;
+};
+
+const showHistory = () => {
+  timesArr.forEach((time) => {
+    timeList.textContent = "";
+    const newTime = document.createElement("li");
+    newTime.innerHTML = `Pomiar nr X: <span>${time}</span>`;
+
+    timeList.appendChild(newTime);
+  });
+};
 
 startBtb.addEventListener("click", handleStart);
+pauseBtb.addEventListener("click", handlePause);
+stopBtb.addEventListener("click", handleStop);
+resetBtb.addEventListener("click", handleReset);
+historyBtb.addEventListener("click", showHistory);
