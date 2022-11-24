79e6ef9095d30f3909f73ac5f34ab8df74888698
Kamil <jolki33.km@gmail.com>
2022-11-24 22:04:04 +0100

    next sandwich

--- portfolio/sandwitch generator/script.js
+++ portfolio/sandwitch generator/script.js
@@ -1,12 +1,16 @@
+const kcal = [12, 15, 25, 84, 15, 46, 74, 8];
 let chleb = document.querySelector("#chleb");
 const fotoChleb = document.querySelector(".fot-chleb");
 
 const show = () => {
   if (chleb.checked) {
     fotoChleb.style.display = "block";
-    span;
+    chlebtxt.style.textDecoration = "line-through";
+    sum.innerHTML = kcal[0];
   } else {
     fotoChleb.style.display = "none";
+    chlebtxt.style.textDecoration = "none";
+    sum.innerHTML = "";
   }
 };
 
