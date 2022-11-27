37bb1392dec3901215108f80d9ea974d5d5ef4f3
Kamil <jolki33.km@gmail.com>
2022-11-27 14:47:03 +0100

    my

--- portfolio/sandwitch generator/script.js
+++ portfolio/sandwitch generator/script.js
@@ -2,11 +2,18 @@ const kcal = [12, 15, 25, 84, 15, 46, 74, 8];
 let chleb = document.querySelector("#chleb");
 const fotoChleb = document.querySelector(".fot-chleb");
 
-const show = () => {
+let maslo = document.querySelector("#maslo");
+const fotoMaslo = document.querySelector(".fot-maslo");
+
+const chlebFunk = () => {
   if (chleb.checked) {
     fotoChleb.style.display = "block";
     chlebtxt.style.textDecoration = "line-through";
-    sum.innerHTML = kcal[0];
+    if (sum.innerHTML !== "") {
+      sum.innerHTML.parseInt + kcal[0];
+    } else {
+      sum.innerHTML = kcal[0];
+    }
   } else {
     fotoChleb.style.display = "none";
     chlebtxt.style.textDecoration = "none";
@@ -14,4 +21,21 @@ const show = () => {
   }
 };
 
-chleb.addEventListener("click", show);
+const masloFunk = () => {
+  if (maslo.checked) {
+    fotoMaslo.style.display = "block";
+
+    if (sum.innerHTML !== "") {
+      sum.innerHTML.parseInt + kcal[1];
+    } else {
+      sum.innerHTML = kcal[1];
+    }
+  } else {
+    fotoMaslo.style.display = "none";
+
+    sum.innerHTML = "";
+  }
+};
+
+chleb.addEventListener("click", chlebFunk);
+maslo.addEventListener("click", masloFunk);
