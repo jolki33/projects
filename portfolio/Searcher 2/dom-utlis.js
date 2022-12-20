8e0e90bc3493cfabefec83df35031bc54787fad2
Kamil <jolki33.km@gmail.com>
2022-12-20 22:17:37 +0100

    rest api

---
+++ portfolio/Searcher 2/dom-utlis.js
@@ -0,0 +1,74 @@
+const createInfoElement = (labelName, value) => {
+  const infoElement = document.createElement("div");
+
+  const labelElement = document.createElement("strong");
+  labelElement.innerText = `${labelName}: `;
+  const valueElement = document.createElement("span");
+  valueElement.innerText = value;
+
+  infoElement.appendChild(labelElement);
+  infoElement.appendChild(valueElement);
+
+  return infoElement;
+};
+
+const createFlageImgElement = (country) => {
+  const imgContainerElement = document.createElement("div");
+  const imgElement = document.createElement("img");
+  imgElement.src = country.flagUrl;
+  imgElement.alt = `${country.name} flag`;
+
+  imgContainerElement.appendChild(imgElement);
+
+  return imgContainerElement;
+};
+
+const createCountryItemElement = (country) => {
+  const countryElement = document.createElement("li");
+
+  const anchorElement = document.createElement("a");
+  anchorElement.href = `?country=${country.name}`;
+
+  anchorElement.appendChild(createFlageImgElement(country));
+
+  const infoContainerElement = document.createElement("div");
+  infoContainerElement.classList.add("info-container");
+
+  const countryNameElement = document.createElement("strong");
+  countryNameElement.innerText = country.name;
+  countryNameElement.classList.add("country-name");
+
+  infoContainerElement.appendChild(countryNameElement);
+
+  infoContainerElement.appendChild(
+    createInfoElement("Population", country.population)
+  );
+
+  infoContainerElement.appendChild(createInfoElement("Region", country.region));
+
+  infoContainerElement.appendChild(
+    createInfoElement("Capital", country.capital)
+  );
+
+  anchorElement.appendChild(infoContainerElement);
+
+  countryElement.appendChild(anchorElement);
+
+  return countryElement;
+};
+
+const createListElement = (countries) => {
+  const listElement = document.createElement("ul");
+  countries.forEach((country) => {
+    listElement.appendChild(createCountryItemElement(country));
+  });
+
+  return listElement;
+};
+
+export const renderCountriesList = (countries) => {
+  const rootElement = document.querySelector("#root");
+  rootElement.innerHTML = "";
+  rootElement.appendChild(createListElement(countries));
+  //render country items into main element
+};
