d1dec8fdf9e987030974ef916976cf0ca21e4bee
Kamil <jolki33.km@gmail.com>
2022-12-29 21:35:10 +0100

    next madified

--- portfolio/Searcher 2/dom-utlis.js
+++ portfolio/Searcher 2/dom-utlis.js
@@ -66,9 +66,63 @@ const createListElement = (countries) => {
   return listElement;
 };
 
+const createDetailElement = (country) => {
+  // capital: country.capital && country.capital[0],
+  // population: country.population.toLocaleString(),
+  // name: country.name.common,
+  // nativeName: country.name.nativeName,
+  // code: country.cioc,
+  // region: country.region,
+  // subregion: country.subregion,
+  // flagUrl: country.flags.png,
+  // currencies: country.currencies,
+  // languages: country.languages,
+  // tld: country.tld[0],
+  const detailContainerElement = document.createElement("div");
+
+  const flagImgElement = createFlageImgElement(country);
+  const detailNameElement = document.createElement("strong");
+  detailNameElement.innerText = country.name;
+
+  detailContainerElement.appendChild(flagImgElement);
+  detailContainerElement.appendChild(detailNameElement);
+
+  detailContainerElement.appendChild(
+    createInfoElement("Native name", country.nativeName)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Population", country.population)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Region", country.region)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Sub region", country.subregion)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Capital", country.capital)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Top level domain", country.tld)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Currencies", country.currencies)
+  );
+  detailContainerElement.appendChild(
+    createInfoElement("Languages", country.languages)
+  );
+
+  return detailContainerElement;
+};
+
 export const renderCountriesList = (countries) => {
   const rootElement = document.querySelector("#root");
   rootElement.innerHTML = "";
   rootElement.appendChild(createListElement(countries));
-  //render country items into main element
+};
+
+export const renderCountryDetails = (country) => {
+  const rootElement = document.querySelector("#root");
+  rootElement.innerHTML = "";
+  rootElement.appendChild(createDetailElement(country));
 };
--- portfolio/Searcher 2/view-details.js
+++ portfolio/Searcher 2/view-details.js
@@ -1,18 +1,40 @@
+import { renderCountryDetails } from "./dom-utlis.js";
+
 export const renderDetail = () => {
-  const searchParams = new URLSearchParams(window.Location.search);
+  console.log(window.location.search);
+  const searchParams = new URLSearchParams(window.location.search);
   const countryCode = searchParams.get("country");
   if (!countryCode) {
-}
+    goBackToDashboard();
+  }
 
-const API_URL_DETAIL = `https://restcountries.com/v3.1/alpha/${countryCode}`;
-fetch(API_URL_DETAIL)
-.then((res) => res.json())
-.then((res) => {
-    
-    
+  const API_URL_DETAIL = `https://restcountries.com/v3.1/alpha/${countryCode}`;
+  fetch(API_URL_DETAIL)
+    .then((res) => res.json())
+    .then(([country]) => {
+      if (!country) {
+        goBackToDashboard();
+      }
+      country = {
+        capital: country.capital && country.capital[0],
+        population: country.population.toLocaleString(),
+        name: country.name.common,
+        nativeName: Object.values(country.name.nativeName)[0].official,
+        code: country.cioc,
+        region: country.region,
+        subregion: country.subregion,
+        flagUrl: country.flags.png,
+        currencies: Object.values(country.currencies)
+          .map((currency) => currency.name)
+          .join(", "),
+        languages: Object.values(country.languages).join(", "),
+        tld: country.tld[0],
+      };
+
+      renderCountryDetails(country);
+    });
 };
 
 const goBackToDashboard = () => {
-    window.Location.href = "/";
-    
-}
+  window.Location.href = "/";
+};
